dd0ad846071c3a5dc44970b8528a2f586766c413
Vladislav <vsoltanovv@gmail.com>
2023-03-09 20:34:25 +0200

    Добавление сущности BookOrder

--- Models/Entity/BookOrder.cs
+++ Models/Entity/BookOrder.cs
@@ -1,28 +1,30 @@
-﻿namespace BooksShop.Models.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksShop.Models.Entity;
 
 /// <summary>
-/// 
+/// Книги Заказы
 /// </summary>
 public class BookOrder
 {
     /// <summary>
-    /// 
+    /// Id заказа
     /// </summary>
-    public int BookId { get; set; }
+    [Key]
+    public Guid OrderId { get; set; }
 
     /// <summary>
-    /// 
-    /// </summary>
-    public Book Book { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public int OrderId { get; set; }
-
-    /// <summary>
-    /// 
+    /// Навигационное свойство для Order
     /// </summary>
     public Order Order { get; set; }
 
+    /// <summary>
+    /// Id книги
+    /// </summary>
+    public Guid BookId { get; set; }
+
+    /// <summary>
+    /// Навигационное свойство для Book
+    /// </summary>
+    public Book? Book { get; set; }
 }
