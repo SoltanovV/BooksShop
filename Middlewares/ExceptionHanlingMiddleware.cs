66837305c6549f52fdd59a393c54a8d5d0917e52
Vladislav <vsoltanovv@gmail.com>
2023-03-10 00:55:14 +0200

    add Middleware

---
+++ Middlewares/ExceptionHanlingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace BooksShop.Middlewares
+{
+    public class ExceptionHanlingMiddleware
+    {
+        private readonly RequestDelegate _requestDelegate;
+        private readonly ILogger<ExceptionHanlingMiddleware> _logger;   
+
+        public ExceptionHanlingMiddleware(RequestDelegate requestDelegate, 
+            ILogger<ExceptionHanlingMiddleware> logger)
+        {
+            _requestDelegate = requestDelegate;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext http)
+        {
+            try
+            {
+                await _requestDelegate(http);
+            }
+            catch (Exception ex)
+            {
+                await HadleExceptionAsync(http, ex.Message, HttpStatusCode.NotFound, "Ошибка");
+            }
+        }
+
+        private async Task HadleExceptionAsync(HttpContext context, string exMessage, HttpStatusCode statusCode, string message)
+        {
+            _logger.LogError(exMessage);
+            HttpResponse response = context.Response;
+
+            response.ContentType = "application/json";
+            response.StatusCode = (int)statusCode;
+
+            ErrorDto errorDto = new()
+            {
+                StatusCode = (int)statusCode,
+                Message = message
+            };
+
+            string result = JsonSerializer.Serialize(errorDto);
+
+            await response.WriteAsJsonAsync(result);
+        }
+    }
+}
