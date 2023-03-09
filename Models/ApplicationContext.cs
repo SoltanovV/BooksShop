ee3a73e754e52a0249ec8c59e340a1712ae42266
Vladislav <vsoltanovv@gmail.com>
2023-03-09 20:35:48 +0200

    Настройка связей бд

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -1,15 +1,15 @@
 ﻿namespace BooksShop.Models
 {
-    public class  ApplicationContext : DbContext
+    public class ApplicationContext : DbContext
     {
         public DbSet<Book> Books { get; set; } = null!;
         public DbSet<Order> Orders { get; set; } = null!;
         public DbSet<BookOrder> BooksOrders { get; set; } = null!;
 
-        public ApplicationContext(DbContextOptions<ApplicationContext> options) 
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
         {
-            Database.EnsureDeleted();
+            //Database.EnsureDeleted();
             Database.EnsureCreated();
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -17,113 +17,114 @@
 
             var book1 = new Book()
             {
-                Id = 1,
+                BookId = Guid.NewGuid(),
                 Name = "Test1",
                 Release = DateTime.Now
             };
 
             var book2 = new Book()
             {
-                Id = 2,
+                BookId = Guid.NewGuid(),
                 Name = "Test",
                 Release = DateTime.Now
             };
-
             var book3 = new Book()
             {
-                Id = 3,
-                Name = "dsdsdsds",
-                Release = new DateTime(2018, 5, 1, 8, 30, 52)
-            };
-
-
-            var book4 = new Book()
-            {
-
-                Id = 4,
-                Name = "dsdsdsd111",
-                Release = DateTime.Now
-            };
-
-
-            var book5 = new Book()
-            {
-                Id = 5,
+                BookId = Guid.NewGuid(),
                 Name = "Test",
                 Release = DateTime.Now
             };
 
-            var books = new List<Book>() { book1, book2, book3, book4, book5 };
+            //var book3 = new Book()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Name = "dsdsdsds",
+            //    Release = new DateTime(2018, 5, 1, 8, 30, 52)
+            //};
 
-            var order1 = new Order()
-            {
-                Id = 1,
-                NumberOrder = 1,
-                OrderDate = new DateTime(2018, 5, 1, 8, 30, 52)
 
-            };
+            //var book4 = new Book()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Name = "dsdsdsd111",
+            //    Release = DateTime.Now
+            //};
 
-            var order2 = new Order()
-            {
-                Id = 2,
-                NumberOrder = 2,
-                OrderDate = new DateTime(2014, 5, 1, 8, 30, 52)
 
-            };
+            //var book5 = new Book()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Name = "Test",
+            //    Release = DateTime.Now
+            //};
 
-            var order3 = new Order()
-            {
-                Id = 3,
-                NumberOrder = 3,
-                OrderDate = new DateTime(2022, 5, 1, 8, 30, 52) 
+            var books = new List<Book>() { book1, book2, book3};
 
-            };
+            //var order1 = new Order()
+            //{
+            //    OrderId = Guid.NewGuid(),
+            //    OrderDate = new DateTime(2018, 5, 1, 8, 30, 52)
 
-            var order4 = new Order()
-            {
-                Id = 4,
-                NumberOrder = 4,
-                OrderDate = new DateTime(2019, 5, 1, 8, 30, 52)
+            //};
 
-            };
+            //var order2 = new Order()
+            //{
+            //    OrderId = Guid.NewGuid(),
+            //    OrderDate = new DateTime(2014, 5, 1, 8, 30, 52)
 
-            var orders = new List<Order>() { order1, order2, order3, order4 };
+            //};
 
-            var bookOrder1 = new BookOrder()
-            {
-                BookId = 1,
-                OrderId = 1
-            };
+            //var order3 = new Order()
+            //{
+            //    OrderId = Guid.NewGuid(),
+            //    OrderDate = new DateTime(2022, 5, 1, 8, 30, 52)
 
-            var bookOrder2 = new BookOrder()
-            {
-                BookId = 2,
-                OrderId = 1
-            };
+            //};
 
-            var bookOrder3 = new BookOrder()
-            {
-                BookId = 3,
-                OrderId = 1
-            };
+            //var order4 = new Order()
+            //{
+            //    OrderId = Guid.NewGuid(),
+            //    OrderDate = new DateTime(2019, 5, 1, 8, 30, 52)
 
-            var bookOrder4 = new BookOrder()
-            {
-                BookId = 1,
-                OrderId = 3
-            };
+            //};
 
-            var bookOrder5 = new BookOrder()
-            {
-                BookId = 4,
-                OrderId = 2
-            };
+            //var orders = new List<Order>() { order1, order2, order3, order4 };
 
-            var booksOrders = new List<BookOrder>() { bookOrder1, bookOrder2, bookOrder3, bookOrder4, bookOrder5 };
+            //var bookOrder1 = new BookOrder()
+            //{
+            //    BookId = 1,
+            //    OrderId = Guid.Parse("D66BFE23-8CE9-45E1-A213-F30A5F5E3849")
+            //};
+
+            //var bookOrder2 = new BookOrder()
+            //{
+            //    BookId = 2,
+            //    OrderId = Guid.Parse("E01941C8-D3C9-4AE5-A68F-5617FB9AFA24")
+            //};
+
+            //var bookOrder3 = new BookOrder()
+            //{
+            //    BookId = 3,
+            //    OrderId = Guid.Parse("D66BFE23-8CE9-45E1-A213-F30A5F5E3849")
+            //};
+
+            //var bookOrder4 = new BookOrder()
+            //{
+            //    BookId = 2,
+            //    OrderId = Guid.Parse("67E510F7-1ED1-4A7E-9A9F-18768CD2D4BA")
+            //};
+
+            //var bookOrder5 = new BookOrder()
+            //{
+            //    BookId = 4,
+            //    OrderId = Guid.Parse("34C40F17-B533-4805-8BE2-7C14DD61E9A5")
+            //};
+
+            //var booksOrders = new List<BookOrder>() { bookOrder1, bookOrder2, bookOrder3, bookOrder4, bookOrder5 };
 
             modelBuilder.Entity<Book>().HasData(books);
-            modelBuilder.Entity<Order>().HasData(orders);
-            modelBuilder.Entity<BookOrder>().HasData(booksOrders);
+            //modelBuilder.Entity<Order>().HasData(orders);
+            //modelBuilder.Entity<BookOrder>().HasData(booksOrders);
 
             // relationships many-to-many
             modelBuilder.Entity<Book>()
@@ -139,8 +140,10 @@
                     .WithMany(b => b.BookOrder)
                     .HasForeignKey(bo => bo.BookId),
                 j => j
-                    .HasKey(bo => new { bo.BookId, bo.OrderId})
+                    .HasKey(bo => new { bo.OrderId, bo.BookId})
                 );
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
