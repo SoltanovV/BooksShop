2ccc556a6c122f3ed501d0d080cfed393307c96d
Vladislav <vsoltanovv@gmail.com>
2023-03-09 20:33:53 +0200

    Добавлние сущности для Book

--- Models/Entity/Book.cs
+++ Models/Entity/Book.cs
@@ -1,32 +1,32 @@
 ﻿namespace BooksShop.Models.Entity;
 
 /// <summary>
-/// 
+/// Книга
 /// </summary>
 public class Book
 {
     /// <summary>
-    /// 
+    /// Id книги
     /// </summary>
-    public int Id { get; set; }
+    public Guid BookId { get; set; } = Guid.NewGuid();
 
     /// <summary>
-    /// 
+    /// Название книги
     /// </summary>
     public string Name { get; set; } = string.Empty; 
 
     /// <summary>
-    /// 
+    /// Дата выхода книги
     /// </summary>
     public DateTime Release { get; set; }
 
     /// <summary>
-    /// 
+    /// Навигационное свойство для Order
     /// </summary>
-    public IEnumerable<Order> Orders { get; set; }
+    public IEnumerable<Order> Orders { get; set; } 
 
     /// <summary>
-    /// 
+    /// Навигационное свойство для BookOrder
     /// </summary>
     public IEnumerable<BookOrder> BookOrder { get; set; }
 
