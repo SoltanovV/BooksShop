33c07b93b099b8190d67c5f0482a39b334e4f4ac
Vladislav <vsoltanovv@gmail.com>
2023-03-09 20:37:24 +0200

    Настройка маппинга

--- Utilities/AutomapperSettings.cs
+++ Utilities/AutomapperSettings.cs
@@ -4,14 +4,42 @@ namespace BooksShop.Utilities
 {
     public class AutomapperSettings : Profile
     {
-        public static MapperConfiguration RegisterMaps()
+        public AutomapperSettings()
         {
-            var mapperConfig = new MapperConfiguration(config =>
-            {
-                config.CreateMap<BooksOrdersRequest, Book>().ReverseMap();
-                config.CreateMap<BooksOrdersResponces, Book>().ReverseMap();
-            });
-            return mapperConfig;
+            CreateMap<CreateBookRequest, Book>();
+            CreateMap<Book, CreateBookResponce>();
+
+            CreateMap<UpdateBookRequest, Book>();
+            CreateMap<Book, UpdateBookResponce>();
+
+            CreateMap<Book, GerBookResponce>();
+
+
+            CreateMap<CreateOrderRequest, Order>();
+            CreateMap<Order, CreateOrderResponces>();
+
+            CreateMap<UpdateOrderRequest, Order>();
+            CreateMap<Order, UpdateOrderResponces>();
+
+            CreateMap<Order, GetOrderResponce>();
+
         }
+        //public static MapperConfiguration RegisterMaps()
+        //{
+            
+        //    //var mapperConfig = new MapperConfiguration(config =>
+        //    //{
+        //    //    //config.CreateMap<Book, BookResponce>();
+
+        //    //    //config.CreateMap<CreateOrdersRequest, BookOrder>();
+        //    //    //config.CreateMap<BookOrder, CreateOrdersResponces>();
+
+        //    //    config.CreateMap<CreateOrdersRequest, Order>();
+        //    //    config.CreateMap<Order, CreateOrdersResponces>();
+
+
+        //    //});
+        //   //return mapperConfig;
+        //}
     }
 }
