a1a8be4698d003394af23e4878d8abf9e7b867f3
Vladislav <vsoltanovv@gmail.com>
2023-03-09 20:33:21 +0200

    Добавление интерфейсов для работы с Book и Order

--- Interfaces/IBookRepository.cs
+++ Interfaces/IBookRepository.cs
@@ -1,26 +1,54 @@
-﻿namespace BooksShop.Interfaces
+﻿namespace BooksShop.Interfaces;
+
+public interface IBookRepository : IDisposable
 {
-    public interface IBookRepository : IDisposable
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        Task<Book> GetIdAsync(int id);
+    /// <summary>
+    /// Получение информации книги по ID
+    /// </summary>
+    /// <param name="id">Id книги</param>
+    /// <returns>Найденную книгу</returns>
+    Task<Book> GetBookAsync(Guid id);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        Task<IEnumerable<Book>> GetListBooksNameAsync(string name);
+    /// <summary>
+    /// Получение списка книг по названию
+    /// </summary>
+    /// <param name="name">Название кинги</param>
+    /// <returns>Список книг по названию</returns>
+    Task<IEnumerable<Book>> GetBooksAsync(string name);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        Task<IEnumerable<Book>> GetListBooksDateAsync(DateTime time);
-    }
+    /// <summary>
+    /// Получение списка книг по дате выхода
+    /// </summary>
+    /// <param name="date">Дата выхода книги</param>
+    /// <returns>Список книг по дате выхода</returns>
+    Task<IEnumerable<Book>> GetBooksAsync(DateTime date);
+
+    /// <summary>
+    /// Получение списка книг по названию и дате выхода
+    /// </summary>
+    /// <param name="name">Название книги</param>
+    /// <param name="date">Дата выхода</param>
+    /// <returns>Список книг по названию и дате выхода</returns>
+    Task<IEnumerable<Book>> GetBooksAsync(string name, DateTime date);
+
+    /// <summary>
+    /// Создает кингу
+    /// </summary>
+    /// <param name="book">Сущность для создания</param>
+    /// <returns>Созданную книгу</returns>
+    Task<Book> CreateBookAsync(Book book);
+
+    /// <summary>
+    /// Обновление информации по книги
+    /// </summary>
+    /// <param name="book">Модель для обновления</param>
+    /// <returns>Обновленную информацию по книге</returns>
+    Task<Book> UpdateBookAsync(Book book);
+
+    /// <summary>
+    /// Удаляет книгу
+    /// </summary>
+    /// <param name="id">Id книги</param>
+    /// <returns>Удаленную книгу</returns>
+    Task<Book> RemoveBookAsync(Guid id);
 }
--- Interfaces/IOrderRepository.cs
+++ Interfaces/IOrderRepository.cs
@@ -1,26 +1,55 @@
-﻿namespace BooksShop.Interfaces
+﻿namespace BooksShop.Interfaces;
+
+public interface IOrderRepository : IDisposable
 {
-    public interface IOrderRepository : IDisposable
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        Task<Order> GetOrderNumberAsync(int num);
+    /// <summary>
+    /// Получение информации о заказе по ID
+    /// </summary>
+    /// <param name="num">Id заказа</param>
+    /// <returns>Информацию по заказу</returns>
+    Task<IEnumerable<Order>> GetOrderAsync(Guid num);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        Task<IEnumerable<Order>> GetListOrdersDateAsync(DateTime time);
+    /// <summary>
+    /// Получение списка заказов за определенное время 
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns>Список заказов</returns>
+    Task<IEnumerable<Order>> GetOrdersAsync(DateTime date);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        Task<BookOrder> CreateOrdersAsync(BookOrder model);
-    }
+    /// <summary>
+    /// Получение заказа за определенное время 
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    Task<IEnumerable<Order>> GetOrdersAsync(Guid num, DateTime date);
+
+    /// <summary>
+    /// Создание заказа
+    /// </summary>
+    /// <param name="model">Модель для создания заказа</param>
+    /// <param name="booksIds">Список книг</param>
+    /// <returns>Созданный заказ</returns>
+    Task<Order> CreateOrderAsync(Order model, IEnumerable<Guid> booksIds);
+
+    /// <summary>
+    /// Обновление заказа
+    /// </summary>
+    /// <param name="model">Модель для обновления заказа</param>
+    /// <param name="booksIds">Список книг</param>
+    /// <returns>Обновленный заказ</returns>
+    Task<Order> UpdateOrderAsync(Order model, IEnumerable<Guid> booksIds);
+
+    /// <summary>
+    /// Удаление заказа
+    /// </summary>
+    /// <param name="orderId">ID заказа</param>
+    /// <returns>Удаленный заказ</returns>
+    Task<Order> RemoveOrderAsync(Guid orderId);
+
+    /// <summary>
+    /// Удаление книги из заказа
+    /// </summary>
+    /// <param name="orderId">ID заказа</param>
+    /// <returns>Удаленный заказ</returns>
+    Task<BookOrder> RemoveBookInOrderAsync(Guid orderId, Guid bookId);
 }
